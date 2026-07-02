acb4e7b4d0d769fb3e1239d283c56c079e7e5b0b
karim-sebih <karim.sebih@laplateforme.io>
2026-07-02 21:35:33 +0200

    fix footer

--- front/src/layouts/PublicLayout.jsx
+++ front/src/layouts/PublicLayout.jsx
@@ -1,17 +1,17 @@
-import{Outlet} from "react-router";
+import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-export default function PublicLayout(){
-return(
-    <>
-    <Navbar/>
+export default function PublicLayout() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
 
-    <main className="flex-1">
-        <Outlet/>
-    </main>
-    <Footer/>
-    </>
-)
+            <main className="flex-1">
+                <Outlet />
+            </main>
 
+            <Footer />
+        </div>
+    );
 }
